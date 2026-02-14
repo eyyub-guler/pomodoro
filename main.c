ddaada8f1164f6b8adeca51b2d2c24945f2b33cf
eyyub-guler <zbupbibop@gmail.com>
2026-02-14 19:58:22 +0300

    i added the animation files and started planning main function

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ncurses.h>
+#include <time.h>
+
+void pomo_wait();
+void pomo_work();
+void pomo_stat();
+void pomo_animationcenter();
+void scrmv();
+void animation;
+int main(int argc,int* argv ){
+//if normal call pomo_animationcenter;
+// if stat call pomo_stat
+}
