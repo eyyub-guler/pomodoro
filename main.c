897cd7ce17e4705882e7f8ea64be14d22d41f658
eyyub-guler <zbupbibop@gmail.com>
2026-02-19 22:09:57 +0300

    opening screen is done you can pick it i will add a warning screen later

--- main.c
+++ main.c
@@ -2,33 +2,37 @@
 #include <stdlib.h>
 #include <ncurses.h>
 #include <time.h>
+#include <signal.h>
+#include <curses.h>
+#include <ncurses.h>
+#include <string.h>
+#include <locale.h>
 
-
-
+volatile sig_atomic_t signal_status = 0;
 int second = 25*60;
 int second_nonused;
 
-void pomo_countdown();
+void pomo_countdown(int time);
 void pomo_stat();
 void pomo_main(int time);
 void scrmv();
 void animation();
-int screenMiddlex();
-int screenMiddley();
-void prtscr(int *file,int x, int y, int starty, int length);
+void screenwarning();
+void prt_scr(FILE *file,int x, int y, int starty, int length);
 int main(int argc,int* argv ){
 pomo_main(second);
 }
-
-
-void prtscr(FILE *file,int x, int y, int starty, int length){
+void sighandler(int s) {
+    signal_status = s;
+}
+void prt_scr(FILE *file,int x, int y, int starty, int length){
     //get the file and print it to the screen
     int i = 0;
     char line[256];
     while (fgets(line, sizeof(line), file)){
         if(i >= starty && i < starty + length){
             line[strcspn(line, "\n")] = '\0';
-            mvstr(y++, x, line); 
+            mvaddstr(y++, x, line); 
         }
         i++;
     }
@@ -36,18 +40,62 @@ void prtscr(FILE *file,int x, int y, int starty, int length){
 }
 void pomo_main(int time){
     // print the main screen open a while and check the screens size
-    int x = screenMiddlex - 29;
-    int y = screenMiddley - 5;
     FILE *f = fopen("main_screen.txt","r");
     if(f == NULL){
-        printf("you havemissing files on your project check the file named main_screen.txt " );
-        return 0;
+        printf("you have missing files on your project check the file named main_screen.txt " );
+        return;
     }
+    
+    setlocale(LC_ALL, "");
     initscr();
-    // ad a function to check screens length
-    while(1)
-    prtscr(f, x,y,0, 9);
-    refresh();
+    start_color();
+    curs_set(0);
+    nodelay(stdscr, TRUE);
+    signal(SIGINT, sighandler);
+    signal(SIGQUIT, sighandler);
+    
+    int rows ,cols;
     
 
+    use_default_colors(); // arka plan için şart
+    while(1){
+        int ch = getch();
+        getmaxyx(stdscr, rows, cols);
+        int x = cols/2 - 29;
+        int y = rows/2 - 5;
+        if (ch == 'q' || ch == 'Q'){ 
+            // write a quiting function
+            break;
+        } else if (ch == 's'){
+            pomo_stat();
+        } else if (ch == ' '){
+            pomo_countdown(time);
+        }
+        if(rows < 11 || cols < 60){
+            clear();
+            screenwarning();    
+        } else {
+            clear();
+            rewind(f); 
+            prt_scr(f, x,y,0, 9);
+        }
+        refresh();
+        napms(100);
+    }
+    curs_set(1);
+    fclose(f);
+        endwin();
+
+}
+void pomo_stat(){
+
+}
+void pomo_countdown(int time){
+
+}
+void screenwarning(){
+    mvaddch(3,3,'3');
+}
+void prtscr(int *file, int x, int y, int starty, int length)
+{
 }
